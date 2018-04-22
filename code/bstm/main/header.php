fe3367cbd402c383817c83f473301087c3f71b1d
Kevin Dunetz <kdunetz@us.ibm.com>
2018-04-21 21:41:47 -0400

    Testing Devops Pipeline

--- code/bstm/main/header.php
+++ code/bstm/main/header.php
@@ -281,7 +281,7 @@ if (!tep_session_is_registered('user_id')) {
       $id = "id=\"active_menu-nav\"";
       $breadCrumb = "How It Works";
    }
-   echo "<li><a href=\"index.php?view=how_it_works&action=display\" class=\"mainlevel-nav\"" . $id . " >How It Works Test KAD KAD</a></li>\n";
+   echo "<li><a href=\"index.php?view=how_it_works&action=display\" class=\"mainlevel-nav\"" . $id . " >How It Works Test KAD KAD 9:41PM</a></li>\n";
    $id = "";
    if (strpos($_SERVER['REQUEST_URI'], "view=register") !== false)
    {
